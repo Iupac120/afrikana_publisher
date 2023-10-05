03da0528a681a571370afa355f139058b0d69c6f
Iupac120 <oforokpara@gmail.com>
2023-10-05 15:11:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express"
+import dotenv from "dotenv"
+const app = express()
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`app is listening to port ${port}`)
+})
