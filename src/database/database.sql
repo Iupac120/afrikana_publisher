0bc5c1a550098f4952e9b911f0eba3577d216f24
Iupac120 <oforokpara@gmail.com>
2023-10-08 03:59:05 -0700

    add user by social

--- src/database/database.sql
+++ src/database/database.sql
@@ -6,7 +6,11 @@ CREATE TABLE users (
     user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_name TEXT NOT NULL,
     user_email TEXT NOT NULL UNIQUE,
-    user_password TEXT NOT NULL
+    user_password TEXT NOT NULL,
+    google_id TEXT,
+    provider VAR (100),
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL,
 );
 
 INSERT INTO users (user_name,user_email,user_password) VALUES ("okpara", "oforokpara@gmail.com","okpara");
--- src/utils/passport.js
+++ src/utils/passport.js
@@ -2,8 +2,22 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Strategy as facebookStrategy } from "passport-facebook";
 import dotenv from "dotenv"
+import pool from "../database/db";
 dotenv.config()
 
+const getProfile = (profile) => {
+    const {id,emails,displayName,provider} = profile
+    if(emails?.length){
+        const email = emails[0].value
+        return {
+            google_id: id,
+            user_name:displayName,
+            email,
+            provider
+        }
+    }
+    return null
+}
 passport.use(
     new GoogleStrategy({
         clientID:process.env.GOOGLECLIENT_ID,
@@ -11,8 +25,21 @@ passport.use(
         callbackURL:"/auth/google/callback",
         scope:["profile","email"]
     },
-    function (accessToken,refreshToken,profile,callback){
-        callback(null,profile)
+    async (accessToken,refreshToken,profile,callback) => {
+        //callback(null,profile)
+        try{
+            const existingGoogleAccount = await pool.query("SELECT * FROM users WHERE google_id = $1",[profile.id])
+            if(!existingGoogleAccount){
+                const existingEmailAccount = await pool.query("SELECT * FROM users WHERE email = $1",[getProfile(profile).email])
+                if(!existingEmailAccount){
+                    const newAccount = await pool.query("INSERT INTO users WHERE user_name =$1,user_email=$2,google_id =$3 RETURNING *",[getProfile(profile).user_name,getProfile(profile).email,getProfile(profile).google_id])
+                    return callback(null, newAccount)//if the user does not exist 
+                }
+            }
+            return callback(null,existingGoogleAccount)//if the user exists
+        }catch(err){
+            throw new Error(err)
+        }
     }
     )
 )
@@ -26,13 +53,26 @@ passport.use(
             profileFields:['emails','displayName','name','picture']
         },
         async (accessToken,refreshToken,profile,callback) => {
-            callback(null,profile)
+            //callback(null,profile)
+            try{
+                const existingGoogleAccount = await pool.query("SELECT * FROM users WHERE google_id = $1",[profile.id])
+                if(!existingGoogleAccount){
+                    const existingEmailAccount = await pool.query("SELECT * FROM users WHERE email = $1",[getProfile(profile).email])
+                    if(!existingEmailAccount){
+                        const newAccount = await pool.query("INSERT INTO users WHERE user_name =$1,user_email=$2,google_id =$3 RETURNING *",[getProfile(profile).user_name,getProfile(profile).email,getProfile(profile).google_id])
+                        return callback(null, newAccount)//if the user does not exist
+                    }
+                }
+                return callback(null,existingGoogleAccount)//if the user exists
+            }catch(err){
+                throw new Error(err)
+            }
         }
     )
 )
 
-passport.serializeUser((user,done) => {
-    done(null,user)
+passport.serializeUser((user,done) => {//saves data to session
+    done(null,user.id)
 })
 
 passport.deserializeUser((user,done)=> {
